aef8118b5f9b652e6ba9f349577ae4b3684b6c9d
Beatriz Cristina Ferreira Cannaval <cannavalbeatriz@gmail.com>
2026-04-15 13:42:39 -0300

    Update EX_01.cpp

--- EX_01.cpp
+++ EX_01.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    string nome, sintoma;
+    int idade;
+    char continuar = 's';
+
+    while (continuar == 's' || continuar == 'S') {
+        cout << "Digite o nome do paciente: ";
+        cin >> nome;
+
+        cout << "Digite a idade: ";
+        cin >> idade;
+
+        cout << "Digite o sintoma: ";
+        cin >> sintoma;
+
+        cout << "\n--- Dados do paciente ---\n";
+        cout << "Nome: " << nome << endl;
+        cout << "Idade: " << idade << endl;
+        cout << "Sintoma: " << sintoma << endl;
+
+        cout << "\nDeseja cadastrar outro paciente? (s/n): ";
+        cin >> continuar;
+    }
+
+    cout << "\nPrograma encerrado.\n";
     return 0;
 }
