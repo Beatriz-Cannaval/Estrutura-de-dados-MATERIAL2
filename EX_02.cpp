a986b0bd83cdd168233d1f6a8f7b3fcc677e6f38
Beatriz Cristina Ferreira Cannaval <cannavalbeatriz@gmail.com>
2026-04-15 13:39:32 -0300

    Update EX_02.cpp

--- EX_02.cpp
+++ EX_02.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    string titulo, autor;
+    int ano;
+
+    while (true) {
+        cout << "Digite o t�tulo do livro (ou 'fim' para encerrar): ";
+        cin >> titulo;
+
+        if (titulo == "fim") {
+            break;
+        }
+
+        cout << "Digite o autor: ";
+        cin >> autor;
+
+        cout << "Digite o ano de publica��o: ";
+        cin >> ano;
+
+        cout << "\n--- Dados do livro ---\n";
+        cout << "T�tulo: " << titulo << endl;
+        cout << "Autor: " << autor << endl;
+        cout << "Ano: " << ano << endl << endl;
+    }
+
+    cout << "Cadastro encerrado.\n";
     return 0;
 }
