b7549ca97b632723361a14cb24b2bc002c8de58b
Beatriz Cristina Ferreira Cannaval <cannavalbeatriz@gmail.com>
2026-04-15 13:43:09 -0300

    Update EX_03.cpp

--- EX_03.cpp
+++ EX_03.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    float enredo, atuacao, efeitos, media;
+    char continuar = 's';
+
+    while (continuar == 's' || continuar == 'S') {
+        cout << "Avalie o ENREDO (0 a 10): ";
+        cin >> enredo;
+
+        cout << "Avalie a ATUACAO (0 a 10): ";
+        cin >> atuacao;
+
+        cout << "Avalie os EFEITOS ESPECIAIS (0 a 10): ";
+        cin >> efeitos;
+
+        media = (enredo + atuacao + efeitos) / 3;
+
+        cout << "\nMedia do filme: " << media << endl;
+
+        cout << "\nDeseja avaliar outro filme? (s/n): ";
+        cin >> continuar;
+    }
+
+    cout << "\nPrograma encerrado.\n";
     return 0;
 }
