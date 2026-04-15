aa70b57acecd5dcfed969c1ebf10d87c639d7eb0
Beatriz Cristina Ferreira Cannaval <cannavalbeatriz@gmail.com>
2026-04-15 13:37:24 -0300

    Update EX_09.cpp

--- EX_09.cpp
+++ EX_09.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+// Criando a enum
+enum Cores {
+    VERMELHO,
+    VERDE,
+    AZUL
+};
+
+// Criando o typedef
+typedef enum Cores TipoCor;
+
+int main() {
+    TipoCor cor;
+    int opcao;
+
+    cout << "Escolha uma cor:\n";
+    cout << "0 - Vermelho\n";
+    cout << "1 - Verde\n";
+    cout << "2 - Azul\n";
+    cin >> opcao;
+
+    cor = (TipoCor) opcao;
+
+    cout << "\nCor escolhida: ";
+
+    if (cor == VERMELHO) {
+        cout << "Vermelho\n";
+    } else if (cor == VERDE) {
+        cout << "Verde\n";
+    } else if (cor == AZUL) {
+        cout << "Azul\n";
+    } else {
+        cout << "Opcao invalida\n";
+    }
+
     return 0;
 }
